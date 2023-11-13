3b0417700b4a06068a6d215ac599bf8fc86738d9
vijaykumardumpeti <vijaydumpeti70@gmail.com>
2023-11-13 13:57:14 +0530

    done-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Transactions from './components/Transactions';
+import './App.css'
+// import Statistics from './components/Statistics';
+// import BarChart from './components/BarChart';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      {/* Render components */}
+      <Transactions />
+      {/* <Statistics />
+      <BarChart /> */}
     </div>
   );
-}
+};
 
 export default App;
